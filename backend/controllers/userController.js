7338d5f582559601b5bcda57f2dfd770542b6425
DavidN250 <onlydavid09@gmail.com>
2023-05-25 20:02:39 +0200

    User Model

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,73 +1,31 @@
-const asyncHandler = require('express-async-handler')
-
-const Goal = require('../models/goalModel')
-
-//@desc   getGoals
-//@route  GET api/getgoals
-//@aCcess Private
-
-const getGoals = asyncHandler(async (req,res) =>{
-    const goals = await Goal.find()
-    res.status(200).json(goals)
-})
 
 //@desc   setGoal
 //@route  POST api/getgoals
 //@acess  Private
+const registerUser = (req,res)=>{
+    res.json({message:'User Created'})
+}
 
-const setGoal = asyncHandler(async (req,res) =>{
-
-    if(!req.body.text){
-        res.status(400)
-        throw new Error('Please add text field')
-    }
-
-    const goal = await Goal.create({
-        text:req.body.text
-    })
-
-   res.status(201).json(goal)
-})
-
-
-//@desc   updateGoal
-//@route  PUT api/getgoals/:id
+//@desc   setGoal
+//@route  POST api/getgoals
 //@acess  Private
-
-const updateGoal = asyncHandler(async (req,res) =>{
-
-    const goal = Goal.findById(req.params.id)
-
-    if(!goal){
-        res.status(404)
-        throw new Error('Goal not found')
-    }
-
-    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body,{new:true})
-
-    res.json(updatedGoal)
-})
+const getUser = (req,res)=>{
+    res.json({message:'User'})
+}
 
 
-//@desc   deleteGoal
-//@route  DELETE api/getgoals/:id
+//@desc   setGoal
+//@route  POST api/getgoals
 //@acess  Private
-
-const deleteGoal = asyncHandler(async (req,res) =>{
-
-    const goal = Goal.findById(req.params.id)
-
-    if(!goal){
-        res.status(404)
-        throw new Error('Goal not found')
-    }
-
-    await Goal.findOneAndDelete(req.params.id)
-    
-    res.json({id:req.params.id})
-})
+const loginUser = (req,res)=>{
+    res.json({message:'User logged in'})
+}
 
 
-module.exports =  {
-    getGoals,setGoal,updateGoal,deleteGoal
+
+
+
+
+module.exports = {
+     registerUser
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+const { registerUser,getUsers } = require('../controllers/userController')
 
 const router = express.Router()
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+// router.route('/').get(getUsers).post(registerUser)
+// router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 // router.get('/',getGoals)
-// router.post('/',setGoal)
+router.post('/',registerUser)
 // router.put('/:id',updateGoal)
 // router.delete('/:id',deleteGoal)
 
